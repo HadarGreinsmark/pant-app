db5566574858fc13cc419ff78c034a5f68d3e556
Hadar Greinsmark <hadarg@kth.se>
2015-11-09 14:21:55 +0100

    Generate Yeoman boilerplate

---
+++ app/scripts/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name pantAppApp
+ * @description
+ * # pantAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('pantAppApp', [
+    'ngAnimate',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
